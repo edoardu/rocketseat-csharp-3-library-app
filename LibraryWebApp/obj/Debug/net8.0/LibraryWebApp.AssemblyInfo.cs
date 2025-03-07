a5377956610a8a644143e87103f609f1f067ff90
Eduardo Rossi <eduardo.rossi.dev@gmail.com>
2025-03-07 11:56:49 -0300

    Initial commit

--- LibraryWebApp/obj/Debug/net8.0/LibraryWebApp.AssemblyInfo.cs
+++ LibraryWebApp/obj/Debug/net8.0/LibraryWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda131f26108fb9bd08b74a360845644c9a8de3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
